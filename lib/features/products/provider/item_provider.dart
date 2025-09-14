faae3c08337e6958c8a9bb2f95facf34dd3aeaa9
Gulshan Kumar giri <golukumarrampur24@gmail.com>
2025-09-14 23:33:27 +0530

    Product is now uploading,showing

--- lib/features/products/provider/item_provider.dart
+++ lib/features/products/provider/item_provider.dart
@@ -3,14 +3,14 @@ import 'package:todo_app/core/database_services/supabase_client_mngr.dart';
 import 'package:todo_app/features/products/data/models/items_model.dart';
 
 class ItemProvider with ChangeNotifier {
-List<ItemsModel> _list = [];
-List<ItemsModel> get listItem => _list;
- // Immediate invoked
- ItemProvider(){
-  fetchItem();
-}
-// Current User Id
-final userId = SupabaseClientManager.client.auth.currentUser?.id;
+  List<ItemsModel> _list = [];
+  List<ItemsModel> get listItem => _list;
+  // Immediate invoked
+  ItemProvider() {
+    fetchItem();
+  }
+  // Current User Id
+  final userId = SupabaseClientManager.client.auth.currentUser?.id;
 
   // Fetch the Items
   Future<void> fetchItem() async {
@@ -24,41 +24,58 @@ final userId = SupabaseClientManager.client.auth.currentUser?.id;
   }
 
   // Create Items
-  Future<void> addItem(String title,String description,String image,int price,int review,String bradName) async{
-   final userID = SupabaseClientManager.client.auth.currentUser?.id;
-    await SupabaseClientManager.client.from('items').insert(
-        {
-          'title': title,
-          'description': description,
-          'images': image,
-          'price': price,
-          'review': review,
-          'brand_name': bradName,
-          'user_id':userID,
-        });
-    await fetchItem();
-
-  }
-
-  // update the itmes
-  Future<void> updateItem(String title,String description,int price,String review,String bradName,DateTime update)async{
-   await SupabaseClientManager.client.from('items').update(
-    {
+  Future<void> addItem(
+    String title,
+    String description,
+    String image,
+    int price,
+    int review,
+    String bradName,
+    String category,
+  ) async {
+    final userID = SupabaseClientManager.client.auth.currentUser?.id;
+    await SupabaseClientManager.client.from('items').insert({
       'title': title,
       'description': description,
       'price': price,
+      'image': image,
       'review': review,
       'brand_name': bradName,
-      'updated_at': DateTime.now(),
-    }
-   ).eq('user_id', userId as String);
-   await fetchItem();
+      'category': category,        // ← Make sure you include this if present
+      'created_at': DateTime.now().toIso8601String(),
+      'user_id': userID,
 
+
+    });
+    await fetchItem();
+  }
+
+  // update the itmes
+  Future<void> updateItem(
+    String title,
+    String description,
+    int price,
+    String review,
+    String bradName,
+    DateTime update,
+  ) async {
+    await SupabaseClientManager.client
+        .from('items')
+        .update({
+          'title': title,
+          'description': description,
+          'price': price,
+          'review': review,
+          'brand_name': bradName,
+          'updated_at': DateTime.now(),
+        })
+        .eq('user_id', userId as String);
+    await fetchItem();
   }
 
   //   delete items
-  Future<void> deleteItem(String id)async{
+  Future<void> deleteItem(String id) async {
     SupabaseClientManager.client.from('items').delete().eq('id', id);
     await fetchItem();
-}
+  }
 }
--- lib/features/products/screens/widgets/addProdItems.dart
+++ lib/features/products/screens/widgets/addProdItems.dart
@@ -1,4 +1,3 @@
-
 import 'dart:io';
 
 import 'package:flutter/material.dart';
@@ -7,7 +6,6 @@ import 'package:todo_app/core/common/pick_image.dart';
 import 'package:todo_app/features/products/provider/item_provider.dart';
 
 class AddItem extends StatefulWidget {
-
   AddItem({super.key});
 
   @override
@@ -16,197 +14,223 @@ class AddItem extends StatefulWidget {
 
 class _AddItemState extends State<AddItem> {
   final title = TextEditingController();
-
   final desc = TextEditingController();
-
   final price = TextEditingController();
-
   final brand = TextEditingController();
-
   final review = TextEditingController();
   final formKey = GlobalKey<FormState>();
 
   File? image;
 
-  void selectImage() async{
+  void selectImage() async {
     final pickedImage = await pickImage();
-    if(pickedImage != null){
+    if (pickedImage != null) {
       setState(() {
         image = pickedImage;
       });
     }
   }
 
+  String category = 'Electronics';
 
-  void uploadProduct() async{
-
-    if(formKey.currentState!.validate() ){
-      final name = title.text.trim();
+  Future<void> uploadProduct() async {
+    if (formKey.currentState!.validate() && image != null) {
+      final titles = title.text.trim();
       final description = desc.text.trim();
       final branding = brand.text.trim();
+
       final reviews = review.text.trim();
-      final prices = price.text.trim();
-       image ;
+      final reviewsInt = int.tryParse(reviews) ?? 0;
+
+      if(reviewsInt>5){
+         ScaffoldMessenger.of(context).showMaterialBanner(
+          MaterialBanner(
+            content: Text('Review cannot be greater than 5'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(
+                    context,
+                  ).hideCurrentMaterialBanner();
+                },
+                child: Text('Close'),
+              ),
+            ],
+            backgroundColor: Colors.red,
+          ),
+        );
+         return;
+      }
+
+      final prices = price.text.trim().replaceAll(',', '');
+
+      final pricesInt = int.tryParse(prices) ?? 0;
+      final img = image!.path;
       final item = Provider.of<ItemProvider>(context, listen: false);
-
-      await item.addItem(name, description,prices,reviews as int ,branding as String,image as String);
-
-
-    };
+      await item.addItem(titles, description, img, pricesInt, reviewsInt, branding,category);
+    }
+    ScaffoldMessenger.of(context).showMaterialBanner(
+      MaterialBanner(
+        content: Text('Item Added'),
+        actions: [
+    TextButton(
+            onPressed: () {
+              ScaffoldMessenger.of(
+                context,
+              ).hideCurrentMaterialBanner();
+            },
+            child: Text('Close'),
+          ),
+        ],
+        backgroundColor: Colors.green,
+      ),
+    );
+    Future.delayed(const Duration(seconds: 1), () {
+      ScaffoldMessenger.of(context).hideCurrentMaterialBanner();
+    });
 
   }
-
-
 
   @override
   Widget build(BuildContext context) {
     final item = Provider.of<ItemProvider>(context);
     return Scaffold(
       appBar: AppBar(title: Text('Add Item Page'), centerTitle: true),
-      body: Container(
-        padding: EdgeInsets.all(20),
-        child: Form(
-          key: formKey,
-          child: Column(
-            spacing: 20,
-            children: [
-              image != null
-                  ? GestureDetector(
-                onTap: selectImage,
-                child: SizedBox(
-                  height: 150,
-                  width: double.infinity,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(16),
-                    child: Image.file(image!, fit: BoxFit.cover),
-                  ),
-                ),
-              )
-                  : GestureDetector(
-                onTap: () {
-                  selectImage();
-                },
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 150,
-                        width: double.infinity,
-                        child: const Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(Icons.folder_open, size: 40),
-                            SizedBox(height: 15),
-                            Text(
-                              "Select Your Image",
-                              style: TextStyle(fontSize: 15),
-                            ),
-                          ],
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Form(
+            key: formKey,
+            child: Column(
+              spacing: 20,
+              children: [
+                image != null
+                    ? GestureDetector(
+                      onTap: selectImage,
+                      child: SizedBox(
+                        height: 200,
+                        width: 200,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(100),
+                          child: Image.file(image!, fit: BoxFit.cover),
                         ),
                       ),
-                    ],),),
-              TextFormField(
-                controller: title,
-                decoration: InputDecoration(
-                  hintText: 'Title',
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.red, width: 1),
-                    borderRadius: BorderRadius.circular(10),
-                    gapPadding: 10.0,
-                  ),
-                ),
-              ),
-              TextFormField(
-                controller: desc,
-                decoration: InputDecoration(
-                  hintText: 'Description',
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.red, width: 1),
-                    borderRadius: BorderRadius.circular(10),
-                    gapPadding: 10.0,
-                  ),
-                ),
-              ),
-              TextFormField(
-                controller: brand,
-                decoration: InputDecoration(
-                  hintText: 'Brand',
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.red, width: 1),
-                    borderRadius: BorderRadius.circular(10),
-                    gapPadding: 10.0,
-                  ),
-                ),
-              ),
-              TextFormField(
-                controller: review,
-                decoration: InputDecoration(
-                  hintText: 'Review',
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.red, width: 1),
-                    borderRadius: BorderRadius.circular(10),
-                    gapPadding: 10.0,
-                  ),
-                ),
-              ),
-              TextFormField(
-                controller: price,
-                decoration: InputDecoration(
-                  hintText: 'Price',
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.red, width: 1),
-                    borderRadius: BorderRadius.circular(10),
-                    gapPadding: 10.0,
-                  ),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: Text('Back'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () async {
-                      // lllllll
-                      uploadProduct();
-                      Future.delayed(Duration(seconds: 1));
-                      ScaffoldMessenger.of(context).showMaterialBanner(MaterialBanner(
-                        content: Text('Item Added'),
-                        actions: [
-                          TextButton(onPressed: (){
-                            ScaffoldMessenger.of(context).hideCurrentMaterialBanner();
-                          }, child: Text('Close'))
-                        ],
-                        backgroundColor: Colors.green,
-                      ));
-
+                    )
+                    : GestureDetector(
+                      onTap: () {
+                        selectImage();
                       },
-                    child: Text('Add Item'),
-
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 150,
+                            width: double.infinity,
+                            child: const Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(Icons.folder_open, size: 40),
+                                SizedBox(height: 15),
+                                Text(
+                                  "Select Your Image",
+                                  style: TextStyle(fontSize: 15),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                TextFormField(
+                  controller: title,
+                  decoration: InputDecoration(
+                    hintText: 'Title',
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red, width: 1),
+                      borderRadius: BorderRadius.circular(10),
+                      gapPadding: 10.0,
+                    ),
                   ),
+                ),
+                TextFormField(
+                  controller: desc,
+                  decoration: InputDecoration(
+                    hintText: 'Description',
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red, width: 1),
+                      borderRadius: BorderRadius.circular(10),
+                      gapPadding: 10.0,
+                    ),
+                  ),
+                ),
+                TextFormField(
+                  controller: brand,
+                  decoration: InputDecoration(
+                    hintText: 'Brand',
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red, width: 1),
+                      borderRadius: BorderRadius.circular(10),
+                      gapPadding: 10.0,
+                    ),
+                  ),
+                ),
+                TextFormField(
+                  controller: review,
+                  decoration: InputDecoration(
+                    hintText: 'Review',
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red, width: 1),
+                      borderRadius: BorderRadius.circular(10),
+                      gapPadding: 10.0,
+                    ),
+                  ),
+                ),
+                TextFormField(
+                  controller: price,
+                  decoration: InputDecoration(
+                    hintText: 'Price',
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red, width: 1),
+                      borderRadius: BorderRadius.circular(10),
+                      gapPadding: 10.0,
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
 
-                ],),
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text('Back'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        uploadProduct();
+                      },
+                      child: Text('Add Item'),
+                    ),
+                  ],
+                ),
 
-              // Column(
-              //   children: [
-              //     image! == null ? GestureDetector(
-              //       onTap: selectImage,
-              //         child: Container(
-              //             height: 100,
-              //             width: 100,
-              //             child: Image.file(image!))) : GestureDetector(
-              //         onTap: (){
-              //           selectImage();
-              //         },
-              //
-              //         child: Text(' Select Image')),
-              //   ],
-              // ),
-            ],
+                // Column(
+                //   children: [
+                //     image! == null ? GestureDetector(
+                //       onTap: selectImage,
+                //         child: Container(
+                //             height: 100,
+                //             width: 100,
+                //             child: Image.file(image!))) : GestureDetector(
+                //         onTap: (){
+                //           selectImage();
+                //         },
+                //
+                //         child: Text(' Select Image')),
+                //   ],
+                // ),
+              ],
+            ),
           ),
         ),
       ),
